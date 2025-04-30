42aca9a60eacf96132d6d58261ced5b8dc5ebe0b
William Jiang <w.jiang156@gmail.com>
2025-04-30 15:16:57 -0500

    change to latest cuda version

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -5,24 +5,19 @@ apt-get upgrade -y
 apt-get install software-properties-common wget curl \
     python3-dev python3-pip python3-wheel python3-setuptools -y
 # install Python packages
-python3 -m pip install --upgrade pip
-pip3 install --user -r .devcontainer/requirements.txt
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv pip install --user -r .devcontainer/requirements.txt
 # update CUDA Linux GPG repository key
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
-dpkg -i cuda-keyring_1.0-1_all.deb
-rm cuda-keyring_1.0-1_all.deb
-# install cuDNN
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
-apt-get update
-apt-get install libcudnn8=8.9.0.*-1+cuda11.8
-apt-get install libcudnn8-dev=8.9.0.*-1+cuda11.8
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+rm cuda-keyring_1.1-1_all.deb
 # install recommended packages
 apt-get install zlib1g g++ freeglut3-dev \
     libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
 # clean up
-pip3 cache purge
+uv pip cache purge
 apt-get autoremove -y
 apt-get clean
+# fun stuff for me 
+curl -sS https://starship.rs/install.sh | sh
+echo $'eval "$(starship init bash)"' >> /root/.bashrc
